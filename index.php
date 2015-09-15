8285c79565854ff5e357975775e25886223ed643
Vladimir Dmitriev <defurator@mail.ru>
2015-09-15 12:49:41 +0300

    header+half footer

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta charset="UTF-8">
-  <title>Start - Главная</title>
+  <title>ESS - Главная</title>
   <link rel="stylesheet" href="css/all.css" type="text/css" />
   <link rel="stylesheet" href="dist/animate.css" type="text/css"/>
   <!--[if lt IE 9]>
@@ -27,14 +27,81 @@
   <script type="text/javascript" src="js/script.js"></script>  
 </head>
 <body>
-  <header class="header">
-    
-  </header>
+  <div class="inner">
+    <header class="header">
+      <div class="inner-internal">
+        <a href="/" class="header_logo">Экспертэнергосервис</a>
+        <div class="header_phone">
+          <p>+375 (17) 399 38 50</p>
+          <p>+375 (29) 66 55 850</p>
+        </div>
+        <div class="header_mail">
+          <p>220004, г. Минск,</p>
+          <p>ул. Короля 9, офис №17</p>
+        </div>
+      </div>
+    </header>
+    <nav class="menu-top">
+      <div class="inner-internal">
+        <ul class="menu-top_list">
+          <li class="menu-top_item">
+            <a href="/" class="menu-top_link">Главная</a>
+          </li>
+          <li class="menu-top_item">
+            <a href="/" class="menu-top_link">О компании</a>
+          </li>
+          <li class="menu-top_item">
+            <span class="menu-top_link">Услуги</span>
+            <ul class="menu-top_list__l2">
+              <li class="menu-top_item__l2">
+                <a href="" class="menu-top_link__l2">Электромонтажные работы</a>
+              </li>
+              <li class="menu-top_item__l2">
+                <a href="" class="menu-top_link__l2">Проектирование</a>
+              </li>
+              <li class="menu-top_item__l2">
+                <a href="" class="menu-top_link__l2">Молниезащита</a>
+              </li>
+              <li class="menu-top_item__l2">
+                <a href="" class="menu-top_link__l2">Электрощитовое оборудование</a>
+              </li>
+              <li class="menu-top_item__l2">
+                <a href="" class="menu-top_link__l2">Оптовая торговля</a>
+              </li>
+            </ul>
+          </li>
+          <li class="menu-top_item">
+            <a href="/" class="menu-top_link">Работы</a>
+          </li>
+          <li class="menu-top_item">
+            <a href="/" class="menu-top_link">Статьи</a>
+          </li>
+          <li class="menu-top_item">
+            <a href="/" class="menu-top_link">Обратная связь</a>
+          </li>
+          <li class="menu-top_item">
+            <a href="/" class="menu-top_link">Контакты</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+tr4t
+      
 
-    
-
-  <footer class="footer">
-    
-  </footer>
+    <footer class="footer">
+      <div class="inner-internal clearfix">
+        <a href="https://webformat.by/" class="dev">Студия "Веб Формат"" - создание и продвижение сайтов</a>
+        <div class="footer_copy"><p>© 2015 Copyright</p></div>
+        <div class="footer_phone">
+          <p>+375 (17) 399 38 50</p>
+          <p>+375 (29) 66 55 850</p>
+        </div>
+        <div class="footer_mail">
+          <p>220004, г. Минск,</p>
+          <p>ул. Короля 9, офис №17</p>
+        </div>
+      </div>
+    </footer>
+  </div>
 </body>
 </html>
